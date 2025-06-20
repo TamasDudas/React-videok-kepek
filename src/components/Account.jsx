f0e607efb1e04ac8a8a09261b99cfb0915f6d82a
Dudás Tamás <ddstms@gmail.com>
2025-06-20 11:21:25 +0200

    Add form submit

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -5,12 +5,16 @@ export default function Account() {
   return (
     <div>
       <h3 className="my-6">Belépés és Regisztráció</h3>
-      <div className=" flex justify-center gap-8 mb-6">
-        <div className="w-1/2">
-          <AccountCard name="Belépés" clear="Törlés" />
+      <div className="grid grid-cols-2 gap-4 mb-6 ">
+        <div>
+          <AccountCard formName="Belépés" btnName="Belépés" clear="Törlés" />
         </div>
-        <div className="w-1/2">
-          <AccountCard name="Küldés" showButton={false} />
+        <div>
+          <AccountCard
+            formName="Regisztráció"
+            btnName="Küldés"
+            isSigin={false}
+          />
         </div>
       </div>
     </div>
--- src/components/AccountCard.jsx
+++ src/components/AccountCard.jsx
@@ -1,44 +1,117 @@
-export default function AccountCard({ name, clear, showButton = true }) {
-  return (
-    <div className="bg-white p-5 shadow-lg rounded-lg max-w-md mx-auto">
-      <h4 className="mb-5 text-center text-lg font-medium">{name}</h4>
-      <div className="mb-4">
-        <label
-          htmlFor="email"
-          className="block mb-1 text-sm font-medium text-gray-700"
-        >
-          email
-        </label>
-        <input
-          type="email"
-          placeholder="email"
-          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-      <div className="mb-4">
-        <label
-          htmlFor="password"
-          className="block mb-1 text-sm font-medium text-gray-700"
-        >
-          password
-        </label>
-        <input
-          type="password"
-          placeholder="password"
-          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-      <div className="flex gap-3 justify-center mt-5">
-        <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors">
-          {name}
-        </button>
+import { useState } from 'react';
 
-        {showButton && (
-          <button className="px-4 py-2 border border-gray-300 bg-white rounded hover:bg-gray-50 transition-colors">
-            {clear}
+export default function AccountCard({
+  formName,
+  btnName,
+  clear,
+  isSigin = true,
+}) {
+  const initialData = {
+    email: '',
+    password: '',
+    checkbox: false,
+  };
+
+  const [formData, setFormData] = useState(initialData);
+
+  const handleData = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    async function userData(credentials) {
+      return fetch(`${isSigin ? '/login' : '/register'}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials),
+      }).then((data) => data.json());
+    }
+    userData({
+      email: formData.email,
+      password: formData.password,
+      ...(isSigin && { checkbox: formData.checkbox }),
+    })
+      .then((data) => {
+        console.log('Sikeres:', data);
+      })
+      .catch((error) => {
+        console.error('Hiba történt:', error);
+      });
+  };
+
+  return (
+    <div className="h-full flex flex-col justify-center">
+      <h4 className="mb-5 text-center text-lg font-medium">{formName}</h4>
+      <form
+        className="bg-white p-5 shadow-lg rounded-lg w-full"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block mb-1 text-sm font-medium text-gray-700"
+          >
+            email
+          </label>
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            value={formData.email}
+            onChange={handleData}
+            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block mb-1 text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            value={formData.password}
+            onChange={handleData}
+            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        {/* Check box remember me vagy üres hely */}
+        <div className="mb-4 flex items-center h-6">
+          {isSigin ? (
+            <>
+              <input
+                type="checkbox"
+                className="mr-2"
+                name="checkbox"
+                checked={formData.checkbox}
+                onChange={handleData}
+              />
+              <label htmlFor="remember" className="text-sm text-gray-700">
+                Emlékezz rám
+              </label>
+            </>
+          ) : (
+            <div></div>
+          )}
+        </div>
+        {/* Gombok */}
+        <div className="flex gap-3 justify-center mt-5">
+          <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors">
+            {btnName}
           </button>
-        )}
-      </div>
+          {isSigin && (
+            <button className="px-4 py-2 border border-gray-300 bg-white rounded hover:bg-gray-50 transition-colors">
+              {clear}
+            </button>
+          )}
+        </div>
+      </form>
     </div>
   );
 }
